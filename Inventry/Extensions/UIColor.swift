b1f6b3ed968a50f792ae6492e78da3dda982a173
Jake Craige <james.craige@gmail.com>
2016-05-30 22:59:23 -0700

    Add tab icons and hack together an app icon and launchscreen

---
+++ Inventry/Extensions/UIColor.swift
@@ -0,0 +1,6 @@
+import UIKit
+
+extension UIColor {
+  static var salmon: UIColor { return .init(red:1.00, green:0.58, blue:0.49, alpha:1.0) }
+  static var shark: UIColor { return .init(red: 0.13, green: 0.16, blue: 0.18, alpha: 1.0) }
+}
